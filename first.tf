93235e432916ed27442d4a77c42fd85933033daa
cloudAni <113286531+cloudAni@users.noreply.github.com>
2023-08-31 12:20:40 +0530

    Update first.tf

--- first.tf
+++ first.tf
@@ -1,11 +1,9 @@
 provider "aws" {
   region     = "us-east-1"
-  access_key = "AKIASXNEBWEYWGQVSGMG"
-  secret_key = "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"
 }
 
 resource "aws_instance" "web" {
-  ami           = "ami-08a52ddb321b32a8c"
+  ami           = "ami-051f7e7f6c2f40dc1"
   instance_type = "t2.micro"
 
   tags = {
